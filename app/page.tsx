e4de6f637212384ad0abaa6f7271baec63ab68e3
Masoud2025 <Masoud.jafary.@outlook.com>
2025-12-29 20:01:18 +0330

    refactor: centralize theme colors and organize UI icons
    - moved background and text colors into global CSS for centralized styling
    - placed previously added icons together on the main page for consistent layout
    - positioned two action buttons side-by-side to improve UI structure

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "../components/Hero";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-[#0C151D] text-[#FFF4CE]">
+    <div className="flex min-h-screen items-center justify-center ">
       <Hero />
     </div>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,33 @@
+import Image from "next/image";
+import Arrow from "../public/icons/Bottom Alignment.svg";
+import GithubIcon from "../public/icons/github.svg";
+import Linkedin from "../public/icons/linkedin.svg";
+import Twitter from "../public/icons/twitter.svg";
+
 export default function Hero() {
   return (
     <div>
-      <h1>Masoud Jafari</h1>
-      <h2>Software Engineer </h2>
+      <h1 className="text-[46px]">Masoud Jafari</h1>
+      <h2 className="text-[22px] text-[#A3ABB2]">Software Engineer </h2>
+      <div className="flex flex-row  gap-3">
+        <Image src={GithubIcon} width={34} height={34} alt="Github icon " />
+        <Image src={Linkedin} width={34} height={34} alt="Linkedin icon " />
+        <Image src={Twitter} width={34} height={34} alt="Linkedin icon " />
+      </div>
+      <p>3 Years of work experience</p>
+      <p>0 Completed projects</p>
+      <p>2 Satisfied customers</p>
+      <div className="flex flex-row gap-4">
+        <button className="w-60 h-15 hover:cursor-pointer bg-[#FFE071] text-black rounded-2xl hover:text-[#A3ABB2] flex flex-row justify-center items-center p-2">
+          Download CV
+          <Image src={Arrow} width={34} height={34} alt="Linkedin icon " />
+        </button>
+        <button className="w-60 h-15 hover:cursor-pointer bg-[#171F26]  rounded-2xl hover:text-[#A3ABB2] p-2">
+          Contact me
+        </button>
+
+        <button></button>
+      </div>
     </div>
   );
 }
