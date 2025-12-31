04b56598449521916d8105ee58d5e03a9e5d4535
Masoud2025 <Masoud.jafary.@outlook.com>
2025-12-31 16:29:12 +0330

    Remove Old styles and start creating new ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
-import ContactMe from "../components/ContacMe";
-import Footer from "../components/Footer";
-import Hero from "../components/Hero";
-import Navbar from "../components/Navbar";
-import Projects from "../components/Projects";
-
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen items-center justify-center ">
-      <Navbar/>
-      <Hero />
-      <Projects/>
-      <ContactMe/>
-      <Footer/>
-    </div>
+    <div className="flex flex-col min-h-screen items-center justify-center "></div>
   );
 }
